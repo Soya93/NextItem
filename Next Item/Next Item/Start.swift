30a4ca473ab24161f005f6147ac66d2a7d65e1ce
Sara <sayo.panda.sn@gmail.com>
2015-11-21 21:08:24 +0100

    First commit!

---
+++ Next Item/Next Item/Start.swift
@@ -0,0 +1,9 @@
+//
+//  Start.swift
+//  Next Item
+//
+//  Created by Sara Nadi on 2015-11-21.
+//  Copyright © 2015 Studiebabes. All rights reserved.
+//
+
+import Foundation
---
+++ Next Item/Next ItemTests/Next_ItemTests.swift
@@ -0,0 +1,36 @@
+//
+//  Next_ItemTests.swift
+//  Next ItemTests
+//
+//  Created by Sara Nadi on 2015-11-21.
+//  Copyright © 2015 Studiebabes. All rights reserved.
+//
+
+import XCTest
+@testable import Next_Item
+
+class Next_ItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
